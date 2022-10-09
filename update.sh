dc8ae4994d8caee87249d8bd2f3eb7d49b994aa2
Muhammad 'Hanzo' Hussain <101168931+itishanzo@users.noreply.github.com>
2022-10-09 11:16:24 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 #Author: Hanzo
 #Created: 31/7/22
-#Modified: 31/7/22
+#Modified: 9/10/22
 
 #Description
 #Lets users  keep their environment updated
@@ -10,10 +10,17 @@
 #Usage
 #update.sh
 
+distro=/etc/os-release
 
-sudo apt -y update 
-sudo apt -y upgrade
 
-if [ -f /var/run/reboot-required ]; then
+if grep  -q "Ubuntu" $distro || grep -q "Debian" $distro; then
+	sudo apt update && sudo apt -y upgrade
+elif grep -q "CentOS" $distro || grep -q "rhel" $distro; then
+	sudo yum update -y
+else
+	echo "Please update this script and add your Distro"
+fi
+
+if [ -f /var/run/reboot-required ]; then # This statement will reboot the system if it required after an update.
 	reboot
 fi
