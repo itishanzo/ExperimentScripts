9c8361bc045965b89e84ec2fca36512da665497c
Hanzo <101168931+itishanzo@users.noreply.github.com>
2026-09-15 07:26:41 +0530

    Update connection.sh

--- connection.sh
+++ connection.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-#Author: Hanzo
-#Created: 27/8/22
-#Modified: 27/8/22
+# Author: Hanzo
+# Created: 27/08/2022
+# Modified: 15/09/2026
 
+# Description:
+# Continuously checks whether a given IP address or hostname is reachable.
 
-#Description
-#This script will run a while loop to check entered IP whether is it Up or Down 
+# Usage:
+# ./connection.sh
 
-#Usage
-#connection.sh
+echo "Hello ${USER^}!"
+read -r -p "Please enter the IP address or hostname to check: " target_ip
 
-echo "Hello ${USER^}, Please enter the IP Address to check whether system is Up or Down"
-read target_ip
+# Check if input is empty
+if [[ -z "$target_ip" ]]; then
+    echo "Error: No IP address or hostname was entered."
+    exit 1
+fi
 
-while true
-do
-	if ping -q -c 2 -W 1 $target_ip > /dev/null; then
-		echo "${USER^}! This IP is up now"
-		break
-	else
-		echo "$target_ip is currently down"
-	fi
+echo
+echo "Checking connectivity to: $target_ip"
+echo "Press Ctrl+C to stop."
+echo
+
+while true; do
+    if ping -q -c 2 -W 1 "$target_ip" > /dev/null 2>&1; then
+        echo "$(date '+%H:%M:%S') - $target_ip is UP"
+        break
+    else
+        echo "$(date '+%H:%M:%S') - $target_ip is DOWN. Retrying in 2 seconds..."
+        sleep 2
+    fi
 done
